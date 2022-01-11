8351701e2ca7f5c9f4dccd7d4b87451ee493d5d2
Maria Lopez <majolopez1991@gmail.com>
2022-01-11 14:45:16 -0500

    change hello world

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 connectDB(process.env.MONGOURI);
 
-app.get('/', (req, res) => res.json({msg:'Welcome to the habits app.. Holi'}));
+app.get('/', (req, res) => res.json({msg:'Welcome to the habits app..'}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT , () => `Server started on port ${PORT}`);
